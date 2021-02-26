7ac0df87834c53d83de37a19159ab0bbc12b696c
ActualEmil <79710867+ActualEmil@users.noreply.github.com>
2021-02-26 19:34:51 +0100

    slight fixes, no duplicate boards

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const board = document.getElementsByTagName("td");
-const HTMLrows = document.getElementsByTagName("tr");
+const HTMLboard = document.getElementsByTagName("td");
 
-var bord  = [[], [], [], [], []];
+var arrayBoard  = [[], [], [], [], []];
 
-for (let i = 0; i < HTMLrows.length; i++) {
-	let children = HTMLrows[i].getElementsByTagName("td");
+for (let i = 0; i < document.getElementsByTagName("tr").length; i++) {
+	let children = document.getElementsByTagName("tr")[i].getElementsByTagName("td");
 	for (let j = 0; j < children.length; j++) {
-		bord[i][j] = children[j];
+		arrayBoard[i][j] = children[j];
 	}
 }
 
-console.log(bord);
+console.log(arrayBoard);
 
 var toggleHighlight = (e) => {
 	e.target.classList.toggle("highlighted");
@@ -21,10 +20,10 @@ var toggleHighlight = (e) => {
 
 var checkForBingo = () => {
 	// Check row
-	for (let i = 0; i < bord.length; i++) {
+	for (let i = 0; i < arrayBoard.length; i++) {
 		let rowIsBingo = true;
-		for (let j = 0; j < bord[i].length; j++) {
-			if (bord[i][j].className != "highlighted") {
+		for (let j = 0; j < arrayBoard[i].length; j++) {
+			if (arrayBoard[i][j].className != "highlighted") {
 				rowIsBingo = false;
 				break;
 			}
@@ -36,12 +35,10 @@ var checkForBingo = () => {
 
 	//check column
 
-	for (let i = 0; i < bord.length; i++) {
+	for (let i = 0; i < arrayBoard.length; i++) {
 		let columnIsBingo = true;
-		for (let j = 0; j < bord[i].length; j++) {
-
-			if (bord[j][i].className != "highlighted") {
-
+		for (let j = 0; j < arrayBoard[i].length; j++) {
+			if (arrayBoard[j][i].className != "highlighted") {
 				columnIsBingo = false;
 				break;
 			}
@@ -50,7 +47,6 @@ var checkForBingo = () => {
 			return true;
 		}
 	}
-
 	return false;
 }
 
@@ -58,12 +54,17 @@ var randint = (limit) => {
 	return Math.floor(Math.random() * limit);
 }
 
-var entries = ["TGVguy", "Someone grinding the Ores route", "Complaints about Historically Inaccurate", "Train deletes itself before spawning",
-"Someone complains about @everyone", "Rokerige Joe", "Someone Complains about signals", "LIGHTS", "TTTE Roleplay"];
+var entries = ["TGVguy", "Soeone grinding the Ores romute", "Complaints about Historically Inaccurate", "Stupid train suggestion", "Train deletes itself before spawning",
+"Someone complains about @everyone", "Rokerige Joe", "Someone Complains about signals", "LIGHTS", "Shiiba posts M Hund", "TTTE Roleplay", "Someone asks when the next update is",
+"Big Dean", "Clock talks about NS 4000", "UnionPacificGuy is retarded in-game", "Cheeselined", "Traffic jam at Zand", "Someone spams whistles", "Missing sounds",
+"Shitty bugreport without F9 screen", "Robin tells someone to boost", "Someone crashes into Zand", "Class 15", "Kid raging about stealing signals"];
 
+console.log(entries.length);
 
-for (let i = 0; i < board.length; i++) {
+for (let i = 0; i < HTMLboard.length; i++) {
 	if (i == 12) continue; // skips functionality for the center square
-	board[i].addEventListener("click", toggleHighlight);
-	board[i].innerText = entries[randint(entries.length)];
+	HTMLboard[i].addEventListener("click", toggleHighlight);
+	let index = randint(entries.length);
+	HTMLboard[i].innerText = entries[index];
+	entries.splice(index, 1)
 }
